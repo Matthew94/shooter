e446081cbb6ceebf1437a0bcaf47d6741b572295
Matthew94 <matthewlove94@gmail.com>
2015-09-28 21:34:50 +0100

    added a door that opens only once
    for hidden walls

---
+++ Assets/Scripts/Usables/Doors/OneShotDoor.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class OneShotDoor : DoorBase
+{
+
+    public override void Activate()
+    {
+        if (state == DoorState.closed)
+        {
+            Open();
+        }
+    }
+
+}
