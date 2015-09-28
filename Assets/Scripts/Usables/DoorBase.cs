f4370b99da038d404c3ccc7866af03b1441ad02f
Matthew94 <matthewlove94@gmail.com>
2015-09-28 16:28:43 +0100

    renaming class as it is now a door base class

---
+++ Assets/Scripts/Usables/DoorBase.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+
+public class DoorBase : UseableObject
+{
+    private enum DoorState
+    {
+        stationary,
+        opening,
+        closing
+    }
+    private DoorState state;
+    private Vector3 closed;
+    private Vector3 open;
+    [SerializeField]
+    private float openTime;
+
+    void Start()
+    {
+
+    }
+
+    public override void Activate()
+    {
+
+    }
+
+
+}
