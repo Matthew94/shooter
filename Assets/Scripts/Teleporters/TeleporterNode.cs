f2caefd75de2c2b3a9f7ef99cce5442599a0426d
Matthew94 <matthewlove94@gmail.com>
2015-09-29 21:33:18 +0100

    simplified teleporters to act as a node

---
+++ Assets/Scripts/Teleporters/TeleporterNode.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class TeleporterNode : MonoBehaviour
+{
+    [SerializeField]
+    private BoxCollider teleporterExit;
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (teleporterExit)
+        {
+            other.transform.position = teleporterExit.transform.position;
+        }
+    }
+
+}
