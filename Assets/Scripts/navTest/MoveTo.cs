8c7b23e5a0130cc461acb74c94d0b0d0fc7bc246
Matthew94 <matthewlove94@gmail.com>
2015-10-11 21:58:45 +0100

    did some of the tutorial

---
+++ Assets/Scripts/navTest/MoveTo.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveTo : MonoBehaviour
+{
+    public Transform goal;
+
+    void Start()
+    {
+        var agent = GetComponent<NavMeshAgent>();
+        agent.destination = goal.position;
+    }
+
+}
